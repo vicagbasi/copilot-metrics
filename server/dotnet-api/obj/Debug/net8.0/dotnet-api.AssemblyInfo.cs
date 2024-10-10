4c030b082d86bc6d99ee31d1df92ea5bd3b60a19
Victor Agbasi <iagbasi@bokf.com>
2024-10-10 09:59:50 -0500

    update server dir

--- server/dotnet-api/obj/Debug/net8.0/dotnet-api.AssemblyInfo.cs
+++ server/dotnet-api/obj/Debug/net8.0/dotnet-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991f43ac1912f8a6038aaf0ca65673657562de9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e5d5ade09704e1b81ea2e0254c4a6c465ccf8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
